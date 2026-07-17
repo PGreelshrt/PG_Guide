4155dd2dbbc254b55d903598b7d64eb6703f5560
PGreelshrt <pgmallpng@gmail.com>
2026-07-17 16:25:59 +0300

    Create contact-config.js

---
+++ contact-config.js
@@ -0,0 +1,8 @@
+
+var CONTACT_CONFIG = {
+    hours: "9h – 21h (UTC+0)",
+    contacts: [
+        { name: "Lucia Telegram",   url: "https://t.me/Lucia8050",      icon: "📱" },
+        { name: "Linda Telegram",  url: "https://t.me/PG_Linda",       icon: "📱" },
+    ]
+};
