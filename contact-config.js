1d05c79f79810071b1b79e97cae037f089419821
PGreelshrt <pgmallpng@gmail.com>
2026-07-17 17:09:25 +0300

    Update contact-config.js

--- contact-config.js
+++ contact-config.js
@@ -1,7 +1,6 @@
 
 var CONTACT_CONFIG = {
-    የስራ ሰዓት (Service Hours)9h – 21h (UTC+3)
-    [በኢትዮጵያ አቆጣጠር፡ ከጠዋቱ 3፡00 - ከምሽቱ 3፡00],
+    hours: "9h – 21h (UTC+3)（በኢትዮጵያ አቆጣጠር፡ ከጠዋቱ 3፡00 - ከምሽቱ 3፡00）",
     contacts: [
         { name: "Lucia Telegram",   url: "https://t.me/Lucia8050",      icon: "📱" },
         { name: "Linda Telegram",  url: "https://t.me/PG_Linda",       icon: "📱" },
